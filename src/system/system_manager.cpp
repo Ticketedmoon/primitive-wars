c94b404143cd14d54347c4a197a5c45413058961
Ticketedmoon <shane.creedon3@gmail.com>
2023-10-07 23:29:34 +0100

    main: fix typo
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- src/system/system_manager.cpp
+++ src/system/system_manager.cpp
@@ -17,8 +17,8 @@ void SystemManager::render(GameProperties& properties)
     std::ranges::filter_view view = m_systemsToRender | std::ranges::views::filter([properties](std::shared_ptr<System>& s) {
         return s->shouldApply(properties);
     });
-    std::vector<std::shared_ptr<System>> systemsToUpdate = std::vector(view.begin(), view.end());
-    for (std::shared_ptr<System>& system : systemsToUpdate)
+    std::vector<std::shared_ptr<System>> systemsToRender = std::vector(view.begin(), view.end());
+    for (std::shared_ptr<System>& system : systemsToRender)
     {
         system->execute();
     }
