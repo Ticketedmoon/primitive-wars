d199fd45a9853fc8b7f9c36cf7c512a4f1e31295
Ticketedmoon <shane.creedon3@gmail.com>
2023-10-07 19:34:50 +0100

    main: remove nlohmann.json install, not needed
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,8 +2,3 @@
 sudo apt-get remove libsfml-dev -y
 sudo apt-get install libsfml-dev -y
 sudo apt-get install libconfig-dev -y
-
-rm -rf libs/
-mkdir -p libs/headers
-cd libs/headers
-wget https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
